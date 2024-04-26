7aba716ad66d724c7bdab01aec7962abf5cbb9b7
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2024-04-26 08:53:30 -0400

    Using flexbox

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your next goal" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your next goal" style={styles.textInput} />
         <Button title="Add Goal" />
       </View>
       <View></View>
@@ -16,5 +16,18 @@ const styles = StyleSheet.create({
   appContainer: {
     padding: 5,
     marginTop: 35,
+    marginLeft: 10,
+    marginRight: 30,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
